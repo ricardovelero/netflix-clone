18b04632618151a010f9654d19a6fa7e9ae4b45a
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-04 09:26:05 +0200

    Add register callback

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import { useState, useCallback } from 'react';
 import Input from "@/components/input";
+import axios from 'axios';
 
 const Auth = () => {
     const [email, setEmail] = useState('')
@@ -11,6 +12,14 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
     }, [])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {email, name, password});
+        } catch (e) {
+            console.log(e);
+        }
+    },[email, name, password]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover">

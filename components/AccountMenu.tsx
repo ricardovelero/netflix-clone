ab1675cf01f0d5d75fe4e58b6baab77ebd29427b
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-05-11 15:37:40 +0200

    Add actual user name

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/userCurrentUser";
 
 interface AccountMenuProps {
     visible?: boolean;
 }
 
-const AccountMenu: React.FC<AccountMenuProps> = ({visible}) => {
-    if (!visible) {
-        return null;
-    }
+const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+    const { data } = useCurrentUser();
+
+    if (!visible) return null;
 
     return (
         <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800">
@@ -16,7 +17,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({visible}) => {
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img className="w-8 rounded-md" src="/images/default-slate.png" alt="" />
                     <p className="text-white text-sm group-hover/item:underline">
-                        Username
+                        {data?.name}
                     </p>
                 </div>
             </div>
